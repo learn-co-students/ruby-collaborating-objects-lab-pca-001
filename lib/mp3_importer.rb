9d45a1d6459e9fa47b4bd65fbc1dbc483c67e88f
Jo Fischer <josephinefischer9@gmail.com>
2020-05-08 16:04:03 -0400

    second commit

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,12 +1,19 @@
 class MP3Importer
 attr_accessor :path
 
-def initialize(path)
-  @path = path
-end
+  def initialize(path)
+    @path = path
+  end
 
-def import(list_of_filenames)
-  list_of_filenames.each{ |filename| Song.new_by_filename(filename) }
-end
+  def files
+    @files = Dir.entries(@path).select do |f|
+      !File.directory?(f)
+    end
+  end
+  def import
+    files.each do |file|
+      Song.new_by_filename(file)
+    end
+  end
 
 end
